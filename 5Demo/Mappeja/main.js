8359bf8958167590a6a3725b812d732c8f58a730
EskoJTH <esjutaha@student.jyu.fi>
2017-11-13 05:12:30 +0200

    demo viimeinen pieni bugifixi

--- 5Demo/Mappeja/main.js
+++ 5Demo/Mappeja/main.js
@@ -5,13 +5,14 @@
  * Kannattaa katsoa tarkemmin yksitttäisten metodien dokumentaatiota jos tarkempi ymmärrys on tarpeen.
  */
 window.onload = function () {
+    console.log(data);
     piirraRastit();
     mymap.setView(new L.LatLng(62.12000, 25.65000), 11);
     listaa();
     let dropTarget1 = document.getElementById("joukkueLista");
     let dropTarget2 = document.getElementById("karttaLista");
-    dropTarget2.addEventListener("dragover", dragging );
-    dropTarget1.addEventListener("dragover", dragging );
+    dropTarget2.addEventListener("dragover", dragging);
+    dropTarget1.addEventListener("dragover", dragging);
     dropTarget2.addEventListener("drop", droppingAddRoute);
     dropTarget1.addEventListener("drop", dropping);
 };
@@ -31,7 +32,7 @@ function dragging(e) {
  * eventti draggauselle.
  * @param e drag eventti.
  */
-function droppingAddRoute(e){
+function droppingAddRoute(e) {
     e.preventDefault();
     let tavara = e.dataTransfer.getData("text");
     // lisätään tämän elementin sisään
@@ -63,22 +64,20 @@ function dropping(e) {
  */
 function listaa() {
     let joukkueLista = document.getElementById("joukkueLista");
-    $.getJSON('data.json', function (data) {
-        for (let i = 0; i < data.joukkueet.length; i++) {
-            let elem = data.joukkueet[i];
-            let joukkue = document.createElement("li");
-            joukkue.style.backgroundColor = Rainbow(i);
-            joukkue.textContent = elem.nimi;
-            joukkue.id = elem.nimi;
-            joukkueLista.appendChild(joukkue);
-            joukkue.setAttribute("draggable", "true");
-            joukkue.addEventListener("dragstart", function (e) {
-                // raahataan datana elementin textContent-attribuutin arvo
-                e.dataTransfer.setData("text/plain", joukkue.id);
-                e.dataTransfer.effectAllowed = "move";
-            });
-        }
-    });
+    for (let i = 0; i < data.joukkueet.length; i++) {
+        let elem = data.joukkueet[i];
+        let joukkue = document.createElement("li");
+        joukkue.style.backgroundColor = Rainbow(i);
+        joukkue.textContent = elem.nimi;
+        joukkue.id = elem.nimi;
+        joukkueLista.appendChild(joukkue);
+        joukkue.setAttribute("draggable", "true");
+        joukkue.addEventListener("dragstart", function (e) {
+            // raahataan datana elementin textContent-attribuutin arvo
+            e.dataTransfer.setData("text/plain", joukkue.id);
+            e.dataTransfer.effectAllowed = "move";
+        });
+    }
 
 }
 
